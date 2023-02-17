8680a05199d8b176dc67da86659b3e0d2ed39200
Julia <yagubetslulka@yandex.ru>
2023-02-17 12:36:31 +0300

    data.js

---
+++ data.js
@@ -0,0 +1,100 @@
+const dataApi = `[
+    {
+      "number": "2660",
+      "language": "en",
+      "quote": "Empty heart beats evenly...",
+      "created": "2023-02-06 20:01:18",
+      "shown": "3295",
+      "active": "1",
+      "comment": "Mikhail Lermontov. 'Death of poet'",
+      "image": "https://random.imagecdn.app/500/150"
+    },
+    {
+      "number": "2095",
+      "language": "en",
+      "quote": "Death doesn't wait and life shouldn't wait.",
+      "created": "2023-02-06 18:50:06",
+      "shown": "3113",
+      "active": "1",
+      "comment": "Ivan Turgenev. 'Noble Nest'",
+      "image": "https://random.imagecdn.app/455/155"
+    },
+    {
+      "number": "1956",
+      "language": "en",
+      "quote": "General principles are only in the minds, but in life there are only special cases.",
+      "created": "2023-02-06 18:43:16",
+      "shown": "3154",
+      "active": "1",
+      "comment": "Fedor Dostoevsky. 'Teenager'",
+      "image": "https://random.imagecdn.app/510/150"
+    },
+    {
+      "number": "2287",
+      "language": "en",
+      "quote": "... Money is the only way that brings even a nonentity to the first place.",
+      "created": "2023-02-06 19:19:14",
+      "shown": "3229",
+      "active": "1",
+      "comment": "Fedor Dostoevsky. 'Teenager'",
+      "image": "https://random.imagecdn.app/412/150"
+    },
+    {
+      "number": "2251",
+      "language": "en",
+      "quote": "...Man is looking not so much for God as for miracles.",
+      "created": "2023-02-06 20:26:03",
+      "shown": "3335",
+      "active": "1",
+      "comment": "Fedor Dostoevsky. 'Brothers Karamazov'",
+      "image": "https://random.imagecdn.app/220/153"
+    },
+    {
+      "number": "2546",
+      "language": "en",
+      "quote": "A coward is one who is afraid and runs; and whoever is afraid and does not run is not yet a coward.",
+      "created": "2023-02-06 20:27:41",
+      "shown": "3232",
+      "active": "1",
+      "comment": "Fedor Dostoevsky. 'Idiot'",
+      "image": "https://random.imagecdn.app/405/151"
+    },{
+      "number": "2241",
+      "language": "en",
+      "quote": "The battle is won by the one who is determined to win it!",
+      "created": "2023-02-06 21:00:54",
+      "shown": "3360",
+      "active": "1",
+      "comment": "Lev Tolstoy. 'War and Peace'",
+      "image": "https://random.imagecdn.app/450/110"
+    },
+    {
+      "number": "2256",
+      "language": "en",
+      "quote": "Happiness is not in always doing what you want, but in always wanting what you do.",
+      "created": "2023-02-06 21:17:03",
+      "shown": "3303",
+      "active": "1",
+      "comment": "Lev Tolstoy",
+      "image": "https://random.imagecdn.app/402/120"
+    },
+    {
+      "number": "2252",
+      "language": "en",
+      "quote": "From us as from a tree - or a club, or an icon.",
+      "created": "2023-02-06 21:10:03",
+      "shown": "3294",
+      "active": "1",
+      "comment": "Ivan Bunin. 'Cursed days'",
+      "image": "https://random.imagecdn.app/410/350"
+    },{
+      "number": "235",
+      "language": "en",
+      "quote": "Better a depraved canary than a pious wolf.",
+      "created": "2023-02-06 13:59:06",
+      "shown": "3322",
+      "active": "1",
+      "comment": "Anton Chekhov. 'Not Pernicious Thoughts'",
+      "image": "https://random.imagecdn.app/420/250"
+    }
+  ]`
